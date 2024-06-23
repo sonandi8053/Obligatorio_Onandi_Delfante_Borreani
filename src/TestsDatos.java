40832a01a354286fbdd6dd04c7e73296fe0078ee
santi <sonandi@correo.um.edu.uy>
2024-06-22 21:37:11 -0300

    Tests Unitarios.

--- src/TestsDatos.java
+++ src/TestsDatos.java
@@ -1,4 +1,7 @@
+import exceptions.FechaInvalida;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class TestsDatos {
     public Datos datos = new Datos("universal_top_spotify_songs.csv");
@@ -8,5 +11,23 @@ public class TestsDatos {
         datos.top10PaisDado("UY", "2023-5-10");
     }
 
-    //terminar
+    @Test
+    public void testTop5CancionesQueMasAparecenEnUnDiaDado() throws IOException {
+        datos.top5CancionesQueMasAparecenEnUnDiaDado("2023-01-01");
+    }
+
+    @Test
+    public void testCantidadDeVecesQueApareceUnArtistaEnElTop() throws IOException {
+        datos.cantidadDeVecesQueApareceUnArtistaEnElTop("ArtistaEjemplo", "2023-01-01"); // Ejemplo de artista y fecha
+    }
+
+    @Test
+    public void testTop7ArtistasQueMasAparecen() throws IOException {
+        datos.top7ArtistasQueMasAparecen("2023-01-01", "2023-01-10");
+    }
+
+    @Test
+    public void testCantidadDeCancionesConUnTempoEnUnRangoEspecificoParaUnRangoEspecificoDeFechas() throws IOException, FechaInvalida {
+        datos.cantidadDeCancionesConUnTempoEnUnRangoEspecificoParaUnRangoEspecificoDeFechas("2023-01-01", "2023-01-10", 100.0f, 200.0f); // Ejemplo de fechas y rango de tempo
+    }
 }
